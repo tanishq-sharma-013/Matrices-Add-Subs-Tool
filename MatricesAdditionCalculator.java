e58374b7ad8921c7c4a334a64e6c41243c3eb5c0
tanishq-sharma-013 <163640580+tanishq-sharma-013@users.noreply.github.com>
2024-09-12 18:15:49 +0530

    Update MatricesAdditionCalculator.java

--- MatricesAdditionCalculator.java
+++ MatricesAdditionCalculator.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 public class MatricesAdditionCalculator {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter number of rows: ");
-        int rows = sc.nextInt();
+        int rows = scanner.nextInt();
         System.out.print("Enter number of columns: ");
-        int columns = sc.nextInt();
+        int columns = scanner.nextInt();
         //user input matrix 1
         float matrix1 [][] = new float[rows][columns];
         for(int i=0; i<rows; i++) {
             System.out.println("Enter Row "+(i+1)+" of the 1st Matrix: ");
              for(int j=0; j<matrix1[i].length; j++) {
-                matrix1[i][j] = sc.nextFloat();
+                matrix1[i][j] = scanner.nextFloat();
              }
              
              System.out.println();
@@ -22,21 +22,41 @@ public class MatricesAdditionCalculator {
         for(int i=0; i<rows; i++) {
             System.out.println("Enter Row "+(i+1)+" of the 2nd Matrix: ");
             for(int j=0; j<matrix2[i].length; j++) {
-                matrix2[i][j] = sc.nextFloat();
+                matrix2[i][j] = scanner.nextFloat();
                 
             }
             System.out.println();
         }
+        System.out.print("Enter '+' for additon and '-' for substraction: ");
+        char character = scanner.next().charAt(0);
         //Performs Addition
-        System.out.println("The result of the addition of two matrices is: ");
-        for(int i=0; i<rows; i++) {
-            for(int j=0; j<columns; j++) {
-                result[i][j] = matrix1[i][j] + matrix2[i][j];
-                System.out.print(result[i][j]+" ");
-                
-            }
-            System.out.println();
+        switch(character) {
+            case '+' : 
+                System.out.println("The result of the addition of two matrices is: ");
+                for(int i=0; i<rows; i++) {
+                    for(int j=0; j<columns; j++) {
+                        result[i][j] = matrix1[i][j] + matrix2[i][j];
+                        System.out.print(result[i][j]+" ");
+                        
+                    }
+                    System.out.println();
+                }
+            break;
+            case '-' : 
+                System.out.println("The result of the substraction of two matrices is: ");
+                for(int i=0; i<rows; i++) {
+                    for(int j=0; j<columns; j++) {
+                        result[i][j] = matrix1[i][j] - matrix2[i][j];
+                        System.out.print(result[i][j]+" ");
+                        
+                    }
+                    System.out.println();
+                }    
+            break;
+            default : System.out.println("Invalid Character!");    
         }
+        
+        
     }
 }
 
