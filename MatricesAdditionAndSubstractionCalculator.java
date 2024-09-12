2b1d6d369bad0ad7ff87b022cc55fff9cf4cf0e2
tanishq-sharma-013 <163640580+tanishq-sharma-013@users.noreply.github.com>
2024-09-12 18:16:46 +0530

    Update MatricesAdditionAndSubstractionCalculator.java

--- MatricesAdditionAndSubstractionCalculator.java
+++ MatricesAdditionAndSubstractionCalculator.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class MatricesAdditionCalculator {
+public class MatricesAdditionAndSubstractionCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter number of rows: ");
